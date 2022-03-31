cc4799314694146cb46f6130ba8115a5fa5f040c
hyeongju you <61458960+hyeongju12@users.noreply.github.com>
2022-03-31 16:21:49 +0900

    Add accounts/admin.py User_model

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -17,9 +17,12 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='root.html'), name='root.html'),
+    path('accounts/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
